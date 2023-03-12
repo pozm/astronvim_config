fefc5bbea137271bf38ad0eb7b7675b0be43c770
anna <44528100+pozm@users.noreply.github.com>
2023-03-12 12:47:48 +0000

    add auto install shit

--- plugins/user.lua
+++ plugins/user.lua
@@ -1,12 +1,21 @@
 return {
   -- You can also add new plugins here as well:
   -- Add plugins, the lazy syntax
-  -- "andweeb/presence.nvim",
-  -- {
-  --   "ray-x/lsp_signature.nvim",
-  --   event = "BufRead",
-  --   config = function()
-  --     require("lsp_signature").setup()
-  --   end,
-  -- },
+  "andweeb/presence.nvim",
+  {
+    "ray-x/lsp_signature.nvim",
+    event = "BufRead",
+    config = function()
+      require("lsp_signature").setup()
+    end,
+  },
+  {
+    'saecki/crates.nvim',
+    tag = 'v0.3.0',
+    requires = { 'nvim-lua/plenary.nvim' },
+    config = function()
+      require('crates').setup()
+    end,
+  },
+  { 'rose-pine/neovim', as = 'rose-pine' }
 }
